6f751372050debf17307ceb24d13c54c0b21df5f
Cesar <let5@newpaltz.edu>
2025-02-18 12:35:31 -0500

    update booking and services files

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function fadeOutAndNavigate(page) {
         window.location.href = page; 
     }, 500);
 }
+// services dropdown
+function toggleDropdown(dropdownId) {
+    const dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+}
+
+// selected services function
+function selectServiceList() {
+    const selectedServices = document.querySelectorAll(".dropdown input:checked");
+    const listItem = document.createElementById("selected-services");
+
+    serviceList.innerHTML = ""; // clear previous selected list items
+
+    selectedServices.forEach(service => {
+        let listItem = document.createElement("li");
+        listItem.textContent = service.value;
+        serviceList.appendChild(listItem);
+});
+}
