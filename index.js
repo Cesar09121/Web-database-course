ce7aa089d6b5416f3807c723490c6daeeaac8c3c
Cesar <let5@newpaltz.edu>
2025-04-16 22:25:11 -0400

    install mysql2, delete mysql

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express');
-const User = require('./models/user');
+const path = require('path');
 const app = express();
-const userRoutes = require('./server/routes/user');
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/user", userRoutes);
+// CORS middleware
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");  
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");  
+    next();
+});
+
+// Routes
+const userRoutes = require("./server/routes/user");
+app.use("/api/users", userRoutes);
+
+// Basic route for testing
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
-
-
-
-
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}!!`);
+}).on('error', (err) => {
+    console.error('Server failed to start:', err);
+});
